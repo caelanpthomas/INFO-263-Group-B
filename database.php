4270f24e6e2935249860cc4d035b03e031688cb9
Caelan Thomas <caelanpthomas@gmail.com>
2018-09-21 08:59:39 +1200

    Updated database php
    Initial connection, needs work still

--- database.php
+++ database.php
@@ -1,4 +1,57 @@
 <?php
+
+/**
+ * Returns an invoice from the database with the matching ID, if it exists.
+ *
+ * @param string $id 		The id of the invoice to fetch.
+ *
+ * @return JSON invoices	Returns a json object containing the invoice, or False if no match.
+ */
+function get_invoice_by_id($id, &$error) {
+	
+	// IMPORTANT
+	// FOR TESTING PURPOSES ONLY.
+	$hostname = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "assignmentdb";
+	//
+	
+	// Creating the connection to the MySQL database
+	$conn = new mysqli($hostname, $username, $password, $database);
+
+	// Check if there is an error with the connection.
+	if ($conn->connect_error){
+		$error = $conn->connect_error;
+		return False;
+	}
+	
+	// Preparing and executing the SQL statement
+	$clean_id = sanitize_input($id, $conn);
+	$query = "SELECT * FROM invoice WHERE invoice_id='$clean_id'";
+	
+	// Execute the query
+	$result = $conn->query($query);
+	
+	// Check for errors during execution
+	if (!$result) {
+		$error = $conn->error;
+		return False;
+	}
+	
+	$conn->close();
+	
+	// Initialize array variable
+	$return_array = array();
+	while ($row = $result->fetch_assoc()) {
+		$return_array[] = $row;
+	}
+	
+	// Return array in JSON format
+	return json_encode($return_array);
+}
+
+
 /**
  * Sanitizes an input string.
  *
@@ -12,7 +65,7 @@
  *
  * @return a sanitized string.
  */
-function sanitizeInput($input, $conn)
+function sanitize_input($input, $conn)
 {
     if (get_magic_quotes_gpc()) {
         $input = stripslashes($input);
@@ -20,4 +73,9 @@ function sanitizeInput($input, $conn)
     $input = $conn->real_escape_string($input);
     return htmlentities($input);
 }
+
+$error = "";
+$return_value = get_invoice_by_id("B742632", $error);
+echo $error;
+echo $return_value;
  ?>
